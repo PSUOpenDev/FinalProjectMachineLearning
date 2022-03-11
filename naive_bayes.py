e2ccb993b294d99bea4605692739b0a7728d8ed9
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-03-11 09:40:32 -0800

    add readfile in naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,1 +1,29 @@
-print("naives bayes")
+# ==================================================================================================================== #
+# Contributors: Tri Le + Phuoc Nguyen
+# Description: Naive Bayes
+# Filename: naive_bayes.py
+# ==================================================================================================================== #
+
+# Dependencies
+import math
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# PATH FILES
+DATA_PATH = "./processed_data.csv"
+
+
+def readfile(filename):
+    try:
+        # Read raw dataset
+        raw_data = np.loadtxt(filename, delimiter=",")
+        return raw_data
+
+    # File not found
+    except FileNotFoundError:
+        print("Cannot read files. Please check your dataset!")
+        return None
+
+
+data = readfile(DATA_PATH)
+print(data.shape)
