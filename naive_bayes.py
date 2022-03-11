287c2e07a033e687111322fdf269778817ac5d52
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-03-11 12:11:36 -0800

    solve conflict

--- naive_bayes.py
+++ naive_bayes.py
@@ -110,33 +110,18 @@ def read_file(filename):
         print("Cannot read files. Please check your dataset!")
         return None
 
-<<<<<<< HEAD
-=======
-
-def create_data_set(draw_data):
->>>>>>> 0a6d6c90835d856f50500e7995d1d7aa0bb4c637
 
 def create_data_set(draw_data):
     num_of_col = draw_data.shape[1]
 
-<<<<<<< HEAD
     # get even rows 
     training_set = draw_data[1::2, 0:num_of_col - 1]
-=======
-    # get even rows
-    training_set = draw_data[1::2, 0:num_of_col-1]
->>>>>>> 0a6d6c90835d856f50500e7995d1d7aa0bb4c637
 
     # get even row of the last column
     training_target_set = draw_data[1::2, num_of_col - 1:num_of_col]
 
-<<<<<<< HEAD
     # get even rows 
     testing_set = draw_data[::2, 0:num_of_col - 1]
-=======
-    # get even rows
-    testing_set = draw_data[::2, 0:num_of_col-1]
->>>>>>> 0a6d6c90835d856f50500e7995d1d7aa0bb4c637
 
     # get odd row of last column
     testing_target_set = draw_data[::2, num_of_col -
@@ -178,11 +163,7 @@ def train_and_test(training_set, training_target_set, testing_set, testing_targe
         sum_correct_case = np.sum(diagonal)
         sum_all = np.sum(cfm)
 
-<<<<<<< HEAD
         accuracy = 0 if sum_all == 0 else (sum_correct_case / sum_all * 100)
-=======
-        accuracy = 0 if sum_all == 0 else (sum_correct_case/sum_all * 100)
->>>>>>> 0a6d6c90835d856f50500e7995d1d7aa0bb4c637
 
         if i < testing_set.shape[1]:
             print(
@@ -195,11 +176,6 @@ def train_and_test(training_set, training_target_set, testing_set, testing_targe
     print("## Final accuracy  = ", round(accuracy, 3))
 
 
-<<<<<<< HEAD
-training_set, training_target_set, testing_set, testing_target_set = readfile(DATA_PATH)
-
-train_and_test(training_set, training_target_set, testing_set, testing_target_set)
-=======
 if __name__ == "__main__":
     # PATH FILES
     DATA_PATH = "./processed_data.csv"
@@ -211,4 +187,3 @@ if __name__ == "__main__":
 
     train_and_test(training_set, training_target_set,
                    testing_set, testing_target_set)
->>>>>>> 0a6d6c90835d856f50500e7995d1d7aa0bb4c637
